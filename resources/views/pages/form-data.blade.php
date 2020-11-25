690148a5a9bbf97f95521386cae7528af07e7361
EkaPratama <bukanjoker@rocketmail.com>
2020-11-25 23:52:07 +0700

    fix delete function

--- resources/views/pages/form-data.blade.php
+++ resources/views/pages/form-data.blade.php
@@ -22,6 +22,14 @@ $formTitle = "Data Kata"
             }
         }
         
+        function deleteWord(id) {
+            if (!confirm('Deleted item can`t be restored, are you sure to continue?')) {
+                return
+            }
+            
+            $('#deleteButton-'+id).click()
+        }
+        
     </script>
 @endsection
 
@@ -67,14 +75,15 @@ $formTitle = "Data Kata"
                                     <span>{{$kata->score_relatedness}}</span>
                                 </div>
                                 <div class="">
+                                    <button class="text-danger" onclick="deleteWord({{$kata->id}})">
+                                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                          <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                        </svg>
+                                    </button>
                                     <form action="/words/{{$kata->id}}/delete" method="post">
                                         @csrf
-                                        <button type="submit" class="text-danger" onclick="confirm('Delete can`t be restored, are you sure?');">
-                                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                                              <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                                            </svg>
-                                        </button>
+                                        <button id="deleteButton-{{$kata->id}}" class="d-none" type="submit" name="button"></button>
                                     </form>
                                 </div>
                             </div>
